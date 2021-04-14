89faa87271cfc54671fa72e176184dcbd3e37aa0
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-14 19:04:49 +0300

    mapping thru list

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,20 @@
 import { useState } from 'react'
 
 const Home = () => {
-
-  let [name, setName] = useState('Mario')
-  let [age, setAge] = useState(25)
-
-  let handleClick = () => {
-    setName('Luigi')
-    setAge(30)
-  }
+  let [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum', author: 'mario', id: 1 },
+    { title: 'Welcome party', body: 'second ipsum', author: 'yoshi', id: 2 },
+    { title: 'Web dev top pics', body: 'third ipsum', author: 'mario', id: 3 }
+  ]) 
 
   return (
     <div className='home'>
-      <h1>Homepage</h1>
-      <p>{ name } is { age } years old</p>
-      <button onClick={ handleClick }>Click Me</button>
+      { blogs.map( ( blog ) => (
+        <div className='blog-preview' key = { blog.id }>
+          <h2>{ blog.title }</h2>
+          <p>Writen by { blog.author }</p>
+        </div>
+      ) ) }
     </div>
   );
 }
