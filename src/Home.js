a8a7b58c8e44501c3db003e383cd43a7a6e86214
Pavlin Ivanov <79521032+pavlin-js@users.noreply.github.com>
2021-04-14 13:16:53 +0300

    click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    let handleClick = ( e ) => {
+        console.log("Hello ninjas", e);
+    }
+
+    let handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target);
+    }
+
     return (
         <div className='home'>
             <h1>Homepage</h1>
+            <button onClick={ handleClick }>Click Me</button>
+            <button onClick={ ( e ) => handleClickAgain('Mario', e) }>Click Again</button>
         </div>
     );
 }
